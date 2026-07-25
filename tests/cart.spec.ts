86d1cf4d986f0784a88e38d3d153a14237283050
Abhishek Sharma <abhi21bhardwaj@gmail.com>
2026-07-25 22:49:07 +0100

    added cart tests

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,8 +1,14 @@
 import { CATALOG } from "../data/products";
 import { VALID_USER } from "../data/users";
-import { test } from "../fixtures/app-fixture";
+import { test, expect } from "../fixtures/app-fixture";
 
-test("should add Zara Coat 3 to the cart", { tag: [] }, async ({ app }) => {
-  app.login.loginAs(VALID_USER);
-  app.dashboard.addToCart(CATALOG.ZARA_COAT_3.name);
-});
+test(
+  "should add Zara Coat 3 to the cart",
+  { tag: ["@fixture"] },
+  async ({ app, page }) => {
+    await app.login.navigateTo();
+    await app.login.loginAs(VALID_USER);
+    await expect(page).toHaveURL(/.*dashboard/);
+    app.dashboard.addToCart(CATALOG.ZARA_COAT_3.name);
+  },
+);
